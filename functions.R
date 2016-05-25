773d8775775bf3f9e78b3acd955451631128026c
mhale <mhale027@yahoo.com>
2016-05-25 16:56:58 -0400

    model and functions

--- functions.R
+++ functions.R
@@ -1,6 +1,7 @@
 library(tm)
 library(stringi)
 library(dplyr)
+library(rJava)
 library(RWeka)
 library(NLP)
 library(openNLP)
@@ -29,9 +30,9 @@ close(new)
 rm(new)
 
 
-blogs1 <- blogs[as.logical(rbinom(length(blogs), 1, prob = .001))]
-twitter1 <- twitter[as.logical(rbinom(length(twitter), 1, prob = .001))]
-news1 <- news[as.logical(rbinom(length(news), 1, prob = .001))]
+blogs1 <- blogs[as.logical(rbinom(length(blogs), 1, prob = .01))]
+twitter1 <- twitter[as.logical(rbinom(length(twitter), 1, prob = .01))]
+news1 <- news[as.logical(rbinom(length(news), 1, prob = .01))]
 
 
 sam <- c(blogs1, twitter1, news1)
@@ -40,12 +41,12 @@ sam <- gsub("\ +", " ", sam)
 sam <- tolower(sam)
 sam <- sam[!is.na(sam)]
 
-vc <- VCorpus(VectorSource(sam))
+#vc <- VCorpus(VectorSource(sam))
 
-vc <- tm_map(vc, FUN=stripWhitespace)
-vc <- tm_map(vc, FUN=tolower)
-vc <- tm_map(vc, FUN=removePunctuation)
-vc <- tm_map(vc, FUN=PlainTextDocument)
+#vc <- tm_map(vc, FUN=stripWhitespace)
+#vc <- tm_map(vc, FUN=tolower)
+#vc <- tm_map(vc, FUN=removePunctuation)
+#vc <- tm_map(vc, FUN=PlainTextDocument)
 
 
 unigramizer <- function(sam) NGramTokenizer(sam, RWeka::Weka_control(min=1, max=1))
@@ -109,12 +110,120 @@ V.tri <- nrow(trigrams)
 V.quad <- nrow(quadgrams)
 
 
+tokens <- names(uni.sum)
+bi.tokens <- names(bi.sum)
+bi.count <- sum(as.numeric(bi.sum))
+for (i in 1:length(bi.tokens)){
+      b1[i] <- unlist(stri_split_fixed(bi.tokens[i], " "))[1]
+      b2[i] <- unlist(stri_split_fixed(bi.tokens[i], " "))[2]
+}
+t.bi <- data.frame(term1 = b1, term2 = b2)
+t.bi <- mutate(t.bi, count = as.numeric(bi.sum), prob = as.numeric(bi.sum)/bi.count)
+t.bi$term2 <- as.character(t.bi$term2)
+t.bi$count <- as.numeric(t.bi$count)
+
+tri.tokens <- names(tri.sum)
+tri.count <- sum(as.numeric(tri.sum))
+for (i in 1:length(tri.tokens)){
+      t1[i] <- unlist(stri_split_fixed(tri.tokens[i], " "))[1]
+      t2[i] <- unlist(stri_split_fixed(tri.tokens[i], " "))[2]
+      t3[i] <- unlist(stri_split_fixed(tri.tokens[i], " "))[3]
+}
+t.tri <- data.frame(term1 = t1, term2 = t2, term3 = t3)
+t.tri <- mutate(t.tri, count = as.numeric(tri.sum), prob = as.numeric(count / tri.count))
+t.tri$term3 <- as.character(t.tri$term3)
+t.tri$count <- as.numeric(t.tri$count)
+
+quad.tokens <- names(quad.sum)
+quad.count <- sum(as.numeric(quad.sum))
+for (i in 1:length(quad.tokens)){
+      q1[i] <- unlist(stri_split_fixed(quad.tokens[i], " "))[1]
+      q2[i] <- unlist(stri_split_fixed(quad.tokens[i], " "))[2]
+      q3[i] <- unlist(stri_split_fixed(quad.tokens[i], " "))[3]
+      q4[i] <- unlist(stri_split_fixed(quad.tokens[i], " "))[4]
+}
+t.quad <- data.frame(term1 = q1, term2 = q2, term3 = q3, term4 = q4)
+t.quad <- mutate(t.quad, count = as.numeric(quad.sum), prob = as.numeric(count / quad.count))
+t.quad$term4 <- as.character(t.quad$term4)
+t.quad$count <- as.numeric(t.quad$count)
 
 
 
 
+pkn.bi <- function(input, word){
+      string <- unlist(clean.input(input))
+      num <- length(string)
+      w1 <- string[num]
+      bi.w <- filter(t.bi, term1 == w1)
+      bi.wi <- filter(t.bi, term2 == word)
+      pc.wi <- nrow(bi.wi)/nrow(t.bi)
+      delta <- .5
+      
+      lambda.bi <- (delta / sum(bi.w$count)) * nrow(bi.w)
+      
+      pkn.bi.wi <- (max(c(0, sum(filter(bi.wi, term1 == w1)$count)-delta))) / 
+            sum(bi.w$count) + lambda.bi * pc.wi
+      return(pkn.bi.wi)
+}
+
+pkn.tri <- function(input, word){
+      string <- unlist(clean.input(input))
+      num <- length(string)
+      w1 <- string[num]
+      w2 <- string[num-1]
+      tri.w <- filter(t.tri, term1 == w2, term2 == w1)
+      tri.wi <- filter(t.tri, term3 == word)
+      delta <- .5
+      
+      lambda.tri <- (delta / sum(tri.w$count))*nrow(tri.w)
+      
+      pkn.tri.wi <- (max(c(0, sum(filter(tri.w, term3 == word)$count)-delta)))/ 
+            sum(filter(tri.w, term3 == word)$count) + 
+            lambda.tri*pkn.bi(input, word)
+      return(pkn.tri.wi)
+}
 
 
+pkn.quad <- function(input, word){
+      string <- unlist(clean.input(input))
+      num <- length(string)
+      w1 <- string[num]
+      w2 <- string[num-1]
+      w3 <- string[num-2]
+      quad.w <- filter(t.quad, term1 == w3, term2 == w2, term3 == w1)
+      quad.wi <- filter(t.quad, term4 == word)
+      delta <- .5
+      
+      lambda.quad <- (delta / sum(quad.w$count))*nrow(quad.w)
+      if (sum(filter(quad.w, term4 == word)$count) != 0 & sum(filter(quad.w, term4 == word)$count) !=0) {
+            pkn.quad.wi <- (max(c(0, sum(filter(quad.w, term4 == word)$count)-delta)))/ 
+                  sum(filter(quad.w, term4 == word)$count) + 
+                  lambda.quad*pkn.tri(input, word)
+      } else if (is.na(pkn.tri.wi)) {
+            pkn.quad.wi <- lambda.quad*pkn.bi.wi
+      } else {
+            pkn.quad.wi <- lambda.quad*pkn.tri.wi
+      }
+            
+      return(pkn.quad.wi)
+      
+}
+
+
+prep <- function(input) {
+      string <<- clean.input(input)
+      num <<- length(string)
+      w1 <<- string[num]
+      w2 <<- string[num-1]
+      w3 <<- string[num-2]
+      quad.w <<- filter(t.quad, term1 == w3, term2 == w2, term3 == w1)
+      tri.w <<- filter(t.tri, term1 == w2, term2 == w1)
+      bi.w <<- filter(t.bi, term1 == w1)
+      quad.choices <- head(arrange(quad.w$term4, desc(count)),10)
+      tri.choices <- head(arrange(tri.w$term3, desc(count)),6)
+      bi.choices <- head(arrange(bi.w$term2, desc(count)),4)
+      choices <<- c(quad.chioces, tri.choices, bi.chioces)
+}
 
 
 
@@ -131,9 +240,9 @@ sample.text <- function(blogs, twitter, news) {
 
 clean.input <- function(input) {
     string <- gsub("^", "<s> ", input)
-    string <- gsub("$", " <s>", string)
-    string <- unlist(tolower(stri_split_fixed(string, " ")))
-    string <- gsub("[^a-zA-Z\ <>]|", "", string)
+    string <- tolower(string)
+    string <- unlist(stri_split_fixed(string, " "))
+    string <- gsub("[^a-zA-Z\ <>]", "", string)
     string <- gsub("\ +", " ", string)
     input.string <<- string
 }
@@ -234,5 +343,4 @@ spell.check1 <- function(corpus) {
     
 }
     
-    
-    
+   
--- pkn.R
+++ pkn.R
@@ -1,23 +1,53 @@
 
 
+prep <- function(input) {
+      string <<- clean.input(input)
+      num <<- length(string)
+      if (num == 2) {
+            w1 <<- string[num]
+            w2 <<- "<s>"
+            w3 <<- "<s>"
+      } else if (num == 3) {
+            w1 <<- string[num]
+            w2 <<- string[num-1]
+            w3 <<- "<s>"
+      } else if (num >= 4) {      
+            w1 <<- string[num]
+            w2 <<- string[num-1]
+            w3 <<- string[num-2]
+      } else { 
+            print("Please input 3 or more words")
+            break 
+      }
+      quad.w <<- filter(t.quad, term1 == w3, term2 == w2, term3 == w1)
+      tri.w <<- filter(t.tri, term1 == w2, term2 == w1)
+      bi.w <<- filter(t.bi, term1 == w1)
+      quad.choices <- head(arrange(quad.w, desc(count))$term4,10)
+      tri.choices <- head(arrange(tri.w, desc(count))$term3,6)
+      bi.choices <- head(arrange(bi.w, desc(count))$term2,4)
+      choices <<- c(quad.choices, tri.choices, bi.choices)
+}
+
+
+
 
 
 pkn.bi <- function(input, word){
-      string <- unlist(clean.input(input))
-      num <- length(string)
-      w1 <<- string[num]
-      w2 <<- string[num-1]
-      w3 <<- string[num-2]
-      bi.w <- filter(t.bi, term1 == w1)
-      bi.wi <- filter(t.bi, term2 == word)
-      pc.wi <- nrow(bi.wi)/nrow(t.bi)
-      delta <<- .5
+#      string <<- unlist(clean.input(input))
+#      num <<- length(string)
+#      w1 <<- string[num]
+#      w2 <<- string[num-1]
+#      w3 <<- string[num-2]
+#      bi.w <<- filter(t.bi, term1 == w1)
+      bi.wi <<- filter(t.bi, term2 == word)
+      pc.wi <<- nrow(bi.wi)/nrow(t.bi)
+#      delta <<- .5
       
-      lambda.bi <- (delta / sum(bi.w$count)) * nrow(bi.w)
+      lambda.bi <<- (delta / sum(bi.w$count)) * nrow(bi.w)
       
-      pkn.bi.wi <- (max(c(0, sum(filter(bi.wi, term1 == w1)$count)-delta))) / 
+      pkn.bi.wi <<- (max(c(0, sum(filter(bi.wi, term1 == w1)$count)-delta))) / 
             sum(bi.w$count) + lambda.bi * pc.wi
-      return(pkn.bi.wi)
+#      return(pkn.bi.wi)
 }
 
 pkn.tri <- function(input, word){
@@ -25,16 +55,16 @@ pkn.tri <- function(input, word){
 #      num <- length(string)
 #      w1 <- string[num]
 #      w2 <- string[num-1]
-      tri.w <- filter(t.tri, term1 == w2, term2 == w1)
-      tri.wi <- filter(t.tri, term3 == word)
+#      tri.w <<- filter(t.tri, term1 == w2, term2 == w1)
+      tri.wi <<- filter(t.tri, term3 == word)
 #      delta <- .5
       
-      lambda.tri <- (delta / sum(tri.w$count))*nrow(tri.w)
+      lambda.tri <<- (delta / sum(tri.w$count))*nrow(tri.w)
       
-      pkn.tri.wi <- (max(c(0, sum(filter(tri.w, term3 == word)$count)-delta)))/ 
-            sum(filter(tri.w, term3 == word)$count) + 
+      pkn.tri.wi <<- (max(c(0, sum(filter(tri.w, term3 == word)$count)-delta)))/ 
+            sum(tri.w$count) + 
             lambda.tri*pkn.bi(input, word)
-      return(pkn.tri.wi)
+#      return(pkn.tri.wi)
 }
 
 
@@ -44,22 +74,22 @@ pkn.quad <- function(input, word){
 #      w1 <- string[num]
 #      w2 <- string[num-1]
 #      w3 <- string[num-2]
-      quad.w <- filter(t.quad, term1 == w3, term2 == w2, term3 == w1)
-      quad.wi <- filter(t.quad, term4 == word)
+#      quad.w <<- filter(t.quad, term1 == w3, term2 == w2, term3 == w1)
+      quad.wi <<- filter(t.quad, term4 == word)
 #      delta <- .5
       
-      lambda.quad <- (delta / sum(quad.wi$count))*nrow(quad.w)
+      lambda.quad <<- (delta / sum(quad.w$count))*nrow(quad.w)
       if (sum(filter(quad.w, term4 == word)$count) != 0 & sum(filter(quad.w, term4 == word)$count) !=0) {
-            pkn.quad.wi <- (max(c(0, sum(filter(quad.w, term4 == word)$count)-delta)))/ 
-                  sum(filter(quad.w, term4 == word)$count) + 
+            pkn.quad.wi <<- (max(c(0, sum(filter(quad.w, term4 == word)$count)-delta)))/ 
+                  sum(quad.w$count) + 
                   lambda.quad*pkn.tri(input, word)
       } else if (is.na(pkn.tri.wi)) {
-            pkn.quad.wi <- lambda.quad*pkn.bi.wi
+            pkn.quad.wi <<- lambda.quad*(pkn.bi.wi^2)
       } else {
-            pkn.quad.wi <- lambda.quad*pkn.tri.wi
+            pkn.quad.wi <<- lambda.quad*pkn.tri.wi
       }
       
-      return(pkn.quad.wi)
+#      return(pkn.quad.wi)
       
 }
 
